dba9a22638016ce07fb0cdfbcbbccba1c0a3d536
Sean Patterson <sean@bosson.design>
2019-10-01 20:42:51 -0400

    Initial Commit

---
+++ Christmas Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Christmas Countdown
+//
+//  Created by Sean Patterson on 10/1/19.
+//  Copyright © 2019 Sean Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
