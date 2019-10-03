cfc6266a7607d9e54b6854e7149dcfe3affd4520
Sean Patterson <sean@bosson.design>
2019-10-03 19:52:35 -0400

    first commit

---
+++ Christmas Countdown/SnowFlakeManager.swift
@@ -0,0 +1,9 @@
+//
+//  SnowFlakeManager.swift
+//  Christmas Countdown
+//
+//  Created by Sean Patterson on 10/1/19.
+//  Copyright © 2019 Sean Patterson. All rights reserved.
+//
+
+import Foundation
